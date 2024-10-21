3bdee04ac2ecb0a82d141731b42139b9a040bbfd
Floris Nzabakira <nzafloris@gmail.com>
2024-10-21 23:31:15 +0200

    initial addition for the my dataset and customized faster-rcnn model

---
+++ configs/_base_/datasets/malaria_detection.py
@@ -0,0 +1,40 @@
+# configs/_base_/datasets/malaria_dataset.py
+from mmcv import Config
+
+cfg = Config({
+    'type': 'CocoDataset',
+    'ann_file': 'data/malaria/annotations/train_annotations.json',
+    'img_prefix': 'data/malaria/train/',
+    'pipeline': [
+        # Define your data preprocessing pipeline here
+        dict(type='LoadImageFromFile'),
+        dict(type='LoadAnnotations', with_bbox=True),
+        dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+        dict(type='RandomFlip', flip_ratio=0.5),
+        dict(type='Normalize', 
+             mean=[123.675, 116.28, 103.53], 
+             std=[58.395, 57.12, 57.375], 
+             to_bgr=True),
+        dict(type='Pad', size_divisor=32),
+        dict(type='DefaultFormatBundle'),
+        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    ],
+})
+
+cfg_val = Config({
+    'type': 'CocoDataset',
+    'ann_file': 'data/malaria/annotations/val_annotations.json',
+    'img_prefix': 'data/malaria/val/',
+    'pipeline': [
+        dict(type='LoadImageFromFile'),
+        dict(type='LoadAnnotations', with_bbox=True),
+        dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+        dict(type='Normalize', 
+             mean=[123.675, 116.28, 103.53], 
+             std=[58.395, 57.12, 57.375], 
+             to_bgr=True),
+        dict(type='Pad', size_divisor=32),
+        dict(type='DefaultFormatBundle'),
+        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    ],
+})
