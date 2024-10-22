7b4859156f719d9338cee585fa412b1399164af1
Floris Nzabakira <nzafloris@gmail.com>
2024-10-23 01:10:55 +0200

    solving issues with faster-rcnn model access

--- configs/_custom_models_/malaria_faster_rcnn_v2.py
+++ configs/_custom_models_/malaria_faster_rcnn_v2.py
@@ -106,29 +106,15 @@ train_pipeline = [
     # dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 
-# test_pipeline = [
-#     dict(type='LoadImageFromFile', backend_args=None),
-#     dict(type='Resize', scale=img_scale, keep_ratio=True),
-#     dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375]),
-#     dict(type='Pad', size=img_scale),
-#     dict(type='DefaultFormatBundle'),
-#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-# ]
-
 test_pipeline = [
     dict(type='LoadImageFromFile', backend_args=None),
-    dict(
-        type='MultiScaleFlipAug',
-        transforms=[
-            dict(type='Resize', scale=img_scale, keep_ratio=True),
-            # dict(type='RandomFlip'),
-            dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375]),
-            dict(type='Pad', size=img_scale),
-            dict(type='ImageToTensor', keys=['img']),
-            # dict(type='Collect', keys=['img']),
-        ]
-    )
+    dict(type='Resize', scale=img_scale, keep_ratio=True),
+    dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375]),
+    dict(type='Pad', size=img_scale),
+    # dict(type='DefaultFormatBundle'),
+    # dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
+
 
 train_dataloader = dict(
     batch_size=train_batch_size_per_gpu,
