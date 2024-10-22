b07d7363291f96207e25620cdc6547d069bfa98c
Floris Nzabakira <nzafloris@gmail.com>
2024-10-23 01:09:49 +0200

    solving issues with faster-rcnn model access

--- configs/_custom_models_/malaria_faster_rcnn_v2.py
+++ configs/_custom_models_/malaria_faster_rcnn_v2.py
@@ -125,7 +125,7 @@ test_pipeline = [
             dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375]),
             dict(type='Pad', size=img_scale),
             dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
+            # dict(type='Collect', keys=['img']),
         ]
     )
 ]
