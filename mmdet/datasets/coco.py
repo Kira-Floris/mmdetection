9c3d0eab221fd818f5eb9083b072e9803a807f5b
Floris Nzabakira <nzafloris@gmail.com>
2024-10-22 23:40:25 +0200

    solving issues with faster-rcnn model access

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -114,6 +114,7 @@ class CocoDataset(BaseDetDataset):
         data_info = {}
 
         # TODO: need to change data_prefix['img'] to data_prefix['img_path']
+        print("ERROR Debugging:",self.data_prefix)
         img_path = osp.join(self.data_prefix['img'], img_info['file_name'])
         if self.data_prefix.get('seg', None):
             seg_map_path = osp.join(
