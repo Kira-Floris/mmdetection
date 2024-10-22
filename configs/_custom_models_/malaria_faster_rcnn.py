186bb8e84b5061abe5ca56c257b9caff00fee3a0
Floris Nzabakira <nzafloris@gmail.com>
2024-10-22 20:50:40 +0200

    solving issues with faster-rcnn model access

--- configs/_custom_models_/malaria_faster_rcnn.py
+++ configs/_custom_models_/malaria_faster_rcnn.py
@@ -62,7 +62,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', scale=img_scale, keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomFlip', flipo=True, direction='horizontal'),
     dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=True),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
