85bde4f4f298aadf310d0fd75660fbe438e74b23
Floris Nzabakira <nzafloris@gmail.com>
2024-10-22 12:12:39 +0200

    solving issues with faster-rcnn model access

--- configs/_custom_models_/malaria_faster_rcnn.py
+++ configs/_custom_models_/malaria_faster_rcnn.py
@@ -8,80 +8,49 @@ data = dict(
     train=dict(
         type=dataset_type, 
         ann_file=data_root + 'annotations/train_annotations.json', 
-        img_prefix=data_root + 'train/'
-    ),
+        img_prefix=data_root + 'train/'),
     val=dict(
         type=dataset_type, 
         ann_file=data_root + 'annotations/val_annotations.json', 
-        img_prefix=data_root + 'val/'
-    ),
+        img_prefix=data_root + 'val/'),
     test=dict(
         type=dataset_type, 
         ann_file=data_root + 'annotations/val_annotations.json', 
-        img_prefix=data_root + 'val/'
-    )
+        img_prefix=data_root + 'val/')
 )
 
 # Dataloader settings
 train_dataloader = dict(
     batch_size=2,
     num_workers=2,
-    shuffle=True
-)
+    shuffle=True)
 
 val_dataloader = dict(
     batch_size=1,
     num_workers=2,
-    shuffle=False
-)
+    shuffle=False)
 
 test_dataloader = dict(
     batch_size=1,
     num_workers=2,
-    shuffle=False
-)
+    shuffle=False)
 
 # Optimizer settings
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001),
-    paramwise_cfg=dict(
-        bias_lr_mult=2., bias_decay_mult=0.)
-)
+    paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
 
 # Learning rate schedule
-lr_config = dict(policy='step', step=[16, 22])
+param_scheduler = dict(policy='step', step=[16, 22])
 
-# Total training epochs
-total_epochs = 24
+# Training schedule
+max_epochs = 24
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
 
-# Model configuration (rpn and rcnn go here)
-model = dict(
-    rpn_head=dict(
-        anchor_generator=dict(
-            scales=[8],
-            ratios=[0.5, 1.0, 2.0],
-            strides=[4, 8, 16, 32, 64]
-        ),
-        bbox_coder=dict(
-            type='DeltaXYWHBBoxCoder',
-            target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0]
-        ),
-        loss_cls=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)
-    ),
-    roi_head=dict(
-        bbox_head=dict(
-            num_classes=1,  # Adjust according to the number of classes
-            loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0)
-        )
-    ),
-    # Training settings (train_cfg moved here)
-    train_cfg=dict(
+# RPN and RCNN configurations
+train_cfg.update(
+    dict(
         rpn=dict(
             assigner=dict(
                 type='MaxIoUAssigner',
@@ -89,19 +58,16 @@ model = dict(
                 neg_iou_thr=0.3,
                 min_pos_iou=0.3,
                 match_low_quality=True,
-                ignore_iof_thr=-1
-            ),
+                ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
                 num=256,
                 pos_fraction=0.5,
                 neg_pos_ub=-1,
-                add_gt_as_proposals=False
-            ),
+                add_gt_as_proposals=False),
             allowed_border=0,
             pos_weight=-1,
-            debug=False
-        ),
+            debug=False),
         rcnn=dict(
             assigner=dict(
                 type='MaxIoUAssigner',
@@ -109,46 +75,50 @@ model = dict(
                 neg_iou_thr=0.5,
                 min_pos_iou=0.5,
                 match_low_quality=True,
-                ignore_iof_thr=-1
-            ),
+                ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
                 num=512,
                 pos_fraction=0.25,
                 neg_pos_ub=-1,
-                add_gt_as_proposals=True
-            ),
+                add_gt_as_proposals=True),
             pos_weight=-1,
-            debug=False
-        )
+            debug=False)
     )
 )
 
-# Validation and Test configurations
-val_cfg = dict(interval=1, metric='bbox')
-
+# Validation configuration
 val_evaluator = dict(
-    type='CocoEvaluator',
+    type='CocoEvaluator',  # Use COCO-style evaluator
     ann_file=data_root + 'annotations/val_annotations.json',
-    metric='bbox'
-)
+    metric='bbox')
 
+# Testing configuration
 test_cfg = dict(
     rpn=dict(
         nms_pre=1000,
         max_per_img=1000,
         nms=dict(type='nms', iou_threshold=0.7),
-        min_bbox_size=0
-    ),
+        min_bbox_size=0),
     rcnn=dict(
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.5),
-        max_per_img=100
-    )
+        max_per_img=100)
 )
 
+# Test evaluator configuration
 test_evaluator = dict(
     type='CocoEvaluator',
     ann_file=data_root + 'annotations/val_annotations.json',
-    metric='bbox'
-)
+    metric='bbox')
+
+# Hooks for logging and checkpointing
+default_hooks = dict(
+    checkpoint=dict(interval=5, max_keep_ckpts=2, save_best='auto'),
+    logger=dict(type='LoggerHook', interval=5))
+
+# Visualizer
+visualizer = dict(vis_backends=[dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')])
+
+# Pretrained weights
+load_from = './checkpoints/faster_rcnn_r50_fpn_2x_coco.pth'
