d22d22f7264fa521f3c65bae898279ef31520e93
Floris Nzabakira <nzafloris@gmail.com>
2024-10-22 21:17:35 +0200

    solving issues with faster-rcnn model access

--- configs/_custom_models_/malaria_faster_rcnn.py
+++ configs/_custom_models_/malaria_faster_rcnn.py
@@ -88,7 +88,7 @@ train_dataloader = dict(
         type='CocoDataset',
         data_root=data_root,
         ann_file=train_ann_file,
-        # img_prefix=train_data_prefix,
+        img_prefix=train_data_prefix,
         pipeline=train_pipeline))
 
 val_dataloader = dict(
@@ -101,7 +101,7 @@ val_dataloader = dict(
         type='CocoDataset',
         data_root=data_root,
         ann_file=val_ann_file,
-        # img_prefix=val_data_prefix,
+        img_prefix=val_data_prefix,
         pipeline=val_pipeline))
 
 test_dataloader = val_dataloader
