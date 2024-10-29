5cdbe380f0ba68f55bfa59b61d371d5f8e435b67
Floris Nzabakira <nzafloris@gmail.com>
2024-10-29 23:54:16 +0200

    trying codes provided by mmdetection scratch

--- configs/_base_/datasets/malaria_detection.py
+++ configs/_base_/datasets/malaria_detection.py
@@ -1,40 +1,50 @@
-# configs/_base_/datasets/malaria_dataset.py
-from mmcv import Config
 
-cfg = Config({
-    'type': 'CocoDataset',
-    'ann_file': '/kaggle/working/mmdetection/data/malaria/annotations/train_annotations.json',
-    'img_prefix': '/kaggle/working/mmdetection/data/malaria/train/',
-    'pipeline': [
-        # Define your data preprocessing pipeline here
-        dict(type='LoadImageFromFile'),
-        dict(type='LoadAnnotations', with_bbox=True),
-        dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-        dict(type='RandomFlip', flip_ratio=0.5),
-        dict(type='Normalize', 
-             mean=[123.675, 116.28, 103.53], 
-             std=[58.395, 57.12, 57.375], 
-             to_bgr=True),
-        dict(type='Pad', size_divisor=32),
-        dict(type='DefaultFormatBundle'),
-        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-    ],
-})
-
-cfg_val = Config({
-    'type': 'CocoDataset',
-    'ann_file': '/kaggle/working/mmdetection/data/malaria/annotations/val_annotations.json',
-    'img_prefix': '/kaggle/working/mmdetection/data/malaria/val/',
-    'pipeline': [
-        dict(type='LoadImageFromFile'),
-        dict(type='LoadAnnotations', with_bbox=True),
-        dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-        dict(type='Normalize', 
-             mean=[123.675, 116.28, 103.53], 
-             std=[58.395, 57.12, 57.375], 
-             to_bgr=True),
-        dict(type='Pad', size_divisor=32),
-        dict(type='DefaultFormatBundle'),
-        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-    ],
-})
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/kaggle/working/mmdetection/data/malaria/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/train_annotations.json',
+        img_prefix=data_root + 'train/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val_annotations.json',
+        img_prefix=data_root + 'val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val_annotations.json',
+        img_prefix=data_root + 'val/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
---
+++ configs/_custom_models_/malaria_faster_rcnn_v3.py
@@ -0,0 +1,25 @@
+_base_ = [
+    '/kaggle/working/mmdetection/configs/_base_/models/faster_rcnn_r50_fpn.py',
+    '/kaggle/working/mmdetection/configs/_base_/datasets/coco_detection.py',
+    '/kaggle/working/mmdetection/configs/_base_/schedules/schedule_1x.py', 
+    '/kaggle/working/mmdetection/configs/_base_/default_runtime.py'
+]
+norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)
+model = dict(
+    backbone=dict(
+        frozen_stages=-1,
+        zero_init_residual=False,
+        norm_cfg=norm_cfg,
+        init_cfg=None),
+    neck=dict(norm_cfg=norm_cfg),
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared4Conv1FCBBoxHead',
+            conv_out_channels=256,
+            norm_cfg=norm_cfg)))
+# optimizer
+optimizer = dict(paramwise_cfg=dict(norm_decay_mult=0))
+optimizer_config = dict(_delete_=True, grad_clip=None)
+# learning policy
+lr_config = dict(warmup_ratio=0.1, step=[65, 71])
+runner = dict(type='EpochBasedRunner', max_epochs=73)
